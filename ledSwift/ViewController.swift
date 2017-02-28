9344b802b7d7b4c05ba263d9834484f502a02af8
Duc Viet <nguyenvietduc@Ducs-Mac-mini.local>
2017-02-28 14:20:48 +0700

    Initial Commit

---
+++ ledSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ledSwift
+//
+//  Created by Duc Nguyen Viet on 2/28/17.
+//  Copyright © 2017 Duc Nguyen Viet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
